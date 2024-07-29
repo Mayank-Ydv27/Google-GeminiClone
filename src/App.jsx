271cbf1d93018fb87b9640e5aa9a3c06187f71b2
Mayank Yadav <mayankyadavg2003@gmail.com>
2024-07-30 00:28:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+
+const App = () => {
+  return (
+    <div >
+      <div>
+      <Sidebar className="sidebar"/>
+      </div> 
+      <div className='main-content'>
+        <Main/>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
+
